18715a360f96990261b9509230bb7d5ea56fe04c
nyawawa <ecky2000@gmx.at>
2017-12-30 20:47:21 +0000

    bugfix: check if there is a given argument

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,13 +2,18 @@ class CalculatorController < ApplicationController
   def calculate
     # using params[] "+" is replaced by " " so the original URL must be retrieved and parsed different
     queryString = URI.parse(request.original_url).query
-    expressionString = queryString.split("=")[1] # the service only exprects one argument, so the second part of the string should be the expression 
-    if !expressionString.blank?
-      @calculator = Calculator.new(:expression => expressionString)
-      @calculator.evaluate_expression
-      render :json => @calculator
+    if !queryString.blank?
+      expressionString = queryString.split("=")[1] # the service only exprects one argument, so the second part of the string should be the expression 
+      if !expressionString.blank?
+        @calculator = Calculator.new(:expression => expressionString)
+        @calculator.evaluate_expression
+        render :json => @calculator
+      else
+        render :json => {expression: expressionString, result: "Error: Blank Expression"}
+      end
     else
-      render :json => {expression: expressionString, result: "Error: Blank Expression"}
+      render :json => {expression: "No Argument", result: "No Argument"}
     end
+    
   end
 end
