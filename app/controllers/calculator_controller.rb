6e5b6ddbe059eaedc0d6492ecec35bc4515b30f5
nyawawa <ecky2000@gmx.at>
2017-12-30 10:36:00 +0000

    solve problem where a plus operator is replaced by whitespace in URL

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,13 +1,21 @@
 class CalculatorController < ApplicationController
   def calculate
-    @calculator = Calculator.new(:expression => params[:expression])
+    puts "########## received expression: " + params[:expression]
+    puts "########## start http header"
+    puts request.original_url
+    queryString    = URI.parse(request.original_url).query
+    pp queryString
+    expressionString = queryString.split("=")[1] # the service only exprects one argument set, so the second part of the string should be the expression 
+    puts "########## expression " + expressionString
+    puts "########## end http header"
+    @calculator = Calculator.new(:expression => expressionString)
     if @calculator.save
       puts "########## calling evaluateExpression ..."
       @calculator.evaluate_expression
       puts "########## evaluateExpression done!"
       render :json => @calculator, :only => [:expression, :result]
     else
-      render :json => {expression: params[:expression], result: "Blank Expression"}
+      render :json => {expression: expressionString, result: "Blank Expression"}
     end
   end
 end
